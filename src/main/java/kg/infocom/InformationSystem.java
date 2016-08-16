0d0313a7599211edcef9f8c56f08b6a215860a67
edil <eryspekov@gmail.com>
2016-08-16 09:56:51 +0600

    add info.txt, del organization

---
+++ src/main/java/kg/infocom/InformationSystem.java
@@ -0,0 +1,11 @@
+package kg.infocom;
+
+
+/**
+ * Created by eryspekov on 15.08.16.
+ */
+
+public class InformationSystem {
+
+
+}
--- src/main/java/kg/infocom/Organization.java
+++
@@ -1,7 +0,0 @@
-package kg.infocom;
-
-/**
- * Created by edil on 11.08.2016.
- */
-public class Organization {
-}
